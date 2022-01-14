d64a58abda3076bb9d8483e60401f8abf71f4db9
Debian <debian@soduco-catalog.novalocal>
2022-01-14 15:55:58 +0000

    .env added to CKAN / scripts renamed and stop_all added

---
+++ reverse_proxy_v2/stop_all.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "---"
+echo "stopping reverse-proxy"
+echo "---"
+docker-compose -f nginx/docker-compose.yml stop
+
+echo "---"
+echo "stopping Geonetwork"
+echo "---"
+docker-compose -f geonetwork/docker-compose.yml stop
+
+echo "---"
+echo "stopping CKAN"
+echo "---"
+docker-compose -f ckan/contrib/docker/docker-compose.yml stop
