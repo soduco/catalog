7d0a311e761931ccaa20b0e90c60ba4f0e60feb2
mhersent <melvin.hersent@gmail.com>
2022-08-30 08:21:51 +0000

    git folders reorganized

--- hackMD/README.md
+++
@@ -1,3 +0,0 @@
-markdown files from hackMd
-
-organized by year/month/file.md
--- upload_script.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-
-url = 'https://catalog.geohistoricaldata.org/geonetwork/srv/api/records'
-
-r = requests.get(url)
-print(r)
